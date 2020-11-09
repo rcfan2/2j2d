48c18f15b78feaa5b59aca59dbc4ae34b19ac971
Gcdd1993 <mobile17602526128@gmail.com>
2020-11-09 21:05:09 +0800

    Fix

--- jd_bean_sign.js
+++ jd_bean_sign.js
@@ -51,7 +51,7 @@ if ($.isNode()) {
 async function execSign() {
   console.log(`\n开始执行脚本签到，请稍等`)
   try {
-    if (notify.SCKEY || notify.BARK_PUSH || notify.DD_BOT_TOKEN || (notify.TG_BOT_TOKEN && notify.TG_USER_ID) || notify.IGOT_PUSH_KEY) {
+    if (notify.TG_BOT_TOKEN && notify.TG_USER_ID) {
       await exec("node JD_DailyBonus.js >> result.txt");
     } else {
       // 如果没有提供通知推送，则打印日志
