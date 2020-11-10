e939b02a876a5f8dfd9f29369a4a185cfb09f27a
Gcdd1993 <mobile17602526128@gmail.com>
2020-11-10 21:32:38 +0800

    去除京小超互助码（京小超改版，不需要互助码）

--- shareCodesHelper.js
+++ shareCodesHelper.js
@@ -1,6 +1,6 @@
 // 助力码辅助生成
 class ShareCode {
-    constructor(username, fruits, pet, plantBean, superMarket) {
+    constructor(username, fruits, pet, plantBean) {
         this._username = username
         // 东东农场互助码
         this._fruits = fruits
@@ -8,8 +8,6 @@ class ShareCode {
         this._pet = pet
         // 种豆得豆互助码
         this._plantBean = plantBean
-        // 京小超商圈互助码
-        this._superMarket = superMarket
     }
 
     get username() {
@@ -27,10 +25,6 @@ class ShareCode {
     get plantBean() {
         return this._plantBean
     }
-
-    get superMarket() {
-        return this._superMarket
-    }
 }
 
 let shareCodes = [
@@ -39,7 +33,7 @@ let shareCodes = [
     new ShareCode('18800659641', '987ee04888b64f0597b13049853caca4', 'MTE1NDAxNzgwMDAwMDAwNDAwMzg2Mjk=', 'bknudbr7e4sqwbhh4kxjg7vthwtj3ctybaw66uy'),
     new ShareCode('查钧译', '9a07bef4e7ad49ae8fdaf113de817a1b', 'MTE1NDUyMjEwMDAwMDAwNDAwMTQ1NjM=', 'mlrdw3aw26j3xhxesrqelkqhmqott5jmflm63ha'),
     new ShareCode('刘奕', '800d1ec39dbd473bbcf2f35667b78b02', 'MTAxODc2NTEzNTAwMDAwMDAyOTAxMTkwMQ==', 'q74cnfebbilqdntxbtix6cx5a4'),
-    // new ShareCode('查钧译的老婆', '800d1ec39dbd473bbcf2f35667b78b02', 'MTAxODc2NTEzNTAwMDAwMDAyOTAxMTkwMQ==', 'q74cnfebbilqdntxbtix6cx5a4'),
+    new ShareCode('查钧译的老婆', '', '', 'mlrdw3aw26j3wscxylsmq7u37f2s7oeysmjexxa'),
 ]
 
 // 同一个京东账号的好友互助码用@隔开,不同京东账号互助码用&或者换行隔开
@@ -78,5 +72,3 @@ console.log('=============================================PETSHARECODES=========
 console.log(generate('pet'))
 console.log('=============================================PLANT_BEAN_SHARECODES=================================================')
 console.log(generate('plantBean'))
-console.log('=============================================SUPERMARKET_SHARECODES=================================================')
-console.log(generate('superMarket'))
