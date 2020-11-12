18e53d040c471a94cfff4fecc500340afeb24bf5
Gcdd1993 <mobile17602526128@gmail.com>
2020-11-12 21:14:26 +0800

    ESLint Fix

--- src/utils/shareCodesHelper.js
+++ src/utils/shareCodesHelper.js
@@ -27,21 +27,21 @@ class ShareCode {
   }
 }
 
-let shareCodes = [
+const shareCodes = [
   new ShareCode('小绵羊酱酱', 'f8df73267c104df2acba96d9378f06c5', 'MTE1NDAxNzYwMDAwMDAwMzkxNzczMTE=', 'e7lhibzb3zek2oioxerjozsro55orxh2yys4ula'),
   new ShareCode('18800659641', '987ee04888b64f0597b13049853caca4', 'MTE1NDAxNzgwMDAwMDAwNDAwMzg2Mjk=', 'bknudbr7e4sqwbhh4kxjg7vthwtj3ctybaw66uy'),
   new ShareCode('查钧译', '9a07bef4e7ad49ae8fdaf113de817a1b', 'MTE1NDUyMjEwMDAwMDAwNDAwMTQ1NjM=', 'mlrdw3aw26j3xhxesrqelkqhmqott5jmflm63ha'),
   new ShareCode('刘奕', '800d1ec39dbd473bbcf2f35667b78b02', 'MTAxODc2NTEzNTAwMDAwMDAyOTAxMTkwMQ==', 'q74cnfebbilqdntxbtix6cx5a4'),
   new ShareCode('查钧译的老婆', 'c168e10ee28248e79defbf76bd098e35', 'MTE1NDQ5OTIwMDAwMDAwNDAxNjc4MzU=', 'mlrdw3aw26j3wscxylsmq7u37f2s7oeysmjexxa'),
   new ShareCode('刘奕1', 'd241a66537244934ba66d40701231a82', '', '4npkonnsy7xi3utpy37pb3qc2vwdg4im4ts6rfa'),
-  new ShareCode('hema3210', '4cbf9b4070b14efa853ee3e3ea0a97db', 'MTE1NDUyMjEwMDAwMDAwNDAwMzgwOTU=', 'lqvulo4mnacvkub4mrvr2u3qba'),
+  new ShareCode('hema3210', '4cbf9b4070b14efa853ee3e3ea0a97db', 'MTE1NDUyMjEwMDAwMDAwNDAwMzgwOTU=', 'lqvulo4mnacvkub4mrvr2u3qba')
 ]
 
 // 同一个京东账号的好友互助码用@隔开,不同京东账号互助码用&或者换行隔开
 const generate = (type) => {
   let str = ''
   shareCodes.forEach(shareCode => {
-    let username = shareCode.username
+    const username = shareCode.username
     shareCodes.forEach((it, index1) => {
       if (it[type]) {
         // 不能助力自己
