2b9236f3830df8dbd09268c9f0f4e2672c07dec4
Gcdd1993 <mobile17602526128@gmail.com>
2020-11-12 14:28:18 +0800

    重新整理项目目录

---
+++ src/scripts/jd_blueCoin.js
@@ -0,0 +1,377 @@
+/*
+京小超兑换奖品 脚本地址：https://raw.githubusercontent.com/lxk0301/scripts/master/jd_blueCoin.js
+感谢@yangtingxiao提供PR
+更新时间：2020-11-05
+支持京东多个账号
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+======================quantumultx===============
+[task_local]
+#京小超兑换奖品
+0 0 0 * * * https://raw.githubusercontent.com/lxk0301/scripts/master/jd_blueCoin.js, tag=京小超兑换奖品, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jxc.png, enabled=true
+====================Loon=================
+[Script]
+cron "0 0 0 * * *" script-path=https://raw.githubusercontent.com/lxk0301/scripts/master/jd_blueCoin.js,tag=京小超兑换奖品
+===================Surge==================
+京小超兑换奖品 = type=cron,cronexp="0 0 0 * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/lxk0301/scripts/master/jd_blueCoin.js
+ */
+const {Env} = require('../utils/Env')
+const $ = new Env('京小超兑换奖品');
+const notify = $.isNode() ? require('../utils/sendNotify') : '';
+//Node.js用户请在jdCookie.js处填写京东ck;
+const jdCookieNode = $.isNode() ? require('../utils/jdCookie.js') : '';
+let coinToBeans = $.getdata('coinToBeans') || 0; //兑换多少数量的京豆（20或者1000），0默认兑换不兑换，如需兑换把0改成20或者1000，或者'商品名称'(商品名称放到单引号内)即可
+let jdNotify = false;//是否开启静默运行，默认false关闭(即:奖品兑换成功后会发出通知提示)
+//IOS等用户直接用NobyDa的jd cookie
+let cookiesArr = [], cookie = '';
+if ($.isNode()) {
+  Object.keys(jdCookieNode).forEach((item) => {
+    cookiesArr.push(jdCookieNode[item])
+  })
+  if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {
+  };
+} else {
+  cookiesArr.push($.getdata('CookieJD'));
+  cookiesArr.push($.getdata('CookieJD2'));
+}
+
+const JD_API_HOST = `https://api.m.jd.com/api?appid=jdsupermarket`;
+!(async () => {
+  if (!cookiesArr[0]) {
+    $.msg($.name, '【提示】请先获取cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
+    return;
+  }
+  for (let i = 0; i < cookiesArr.length; i++) {
+    cookie = cookiesArr[i];
+    if (cookie) {
+      $.UserName = decodeURIComponent(cookie.match(/pt_pin=(.+?);/) && cookie.match(/pt_pin=(.+?);/)[1])
+      $.index = i + 1;
+      $.data = {};
+      $.coincount = 0;
+      $.beanscount = 0;
+      $.blueCost = 0;
+      $.coinerr = "";
+      $.beanerr = "";
+      $.title = '';
+      //console.log($.coincount);
+      $.isLogin = true;
+      $.nickName = '';
+      await TotalBean();
+      console.log(`\n开始【京东账号${$.index}】${$.nickName || $.UserName}\n`);
+      if (!$.isLogin) {
+        $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/`, {"open-url": "https://bean.m.jd.com/"});
+        $.setdata('', `CookieJD${i ? i + 1 : ""}`);//cookie失效，故清空cookie。
+        if ($.isNode()) await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
+        continue
+      }
+      //先兑换京豆
+      if ($.isNode()) {
+        if (process.env.MARKET_COIN_TO_BEANS) {
+          coinToBeans = process.env.MARKET_COIN_TO_BEANS;
+        }
+      }
+      if (`${coinToBeans}` !== '0') {
+        await smtgHome();//查询蓝币数量，是否满足兑换的条件
+        await smtg_queryPrize();
+      } else {
+        console.log('查询到您设置的是不兑换京豆选项，现在为您跳过兑换京豆。如需兑换，请去BoxJs设置或者修改脚本coinToBeans\n')
+      }
+      await msgShow();
+    }
+  }
+})()
+  .catch((e) => $.logErr(e))
+  .finally(() => $.done())
+
+//查询任务
+function smtg_queryPrize(timeout = 0) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      let url = {
+        url: `${JD_API_HOST}&functionId=smtg_queryPrize&clientVersion=8.0.0&client=m&body=%7B%7D&t=${Date.now()}`,
+        headers: {
+          'Origin': `https://jdsupermarket.jd.com`,
+          'Cookie': cookie,
+          'Connection': `keep-alive`,
+          'Accept': `application/json, text/plain, */*`,
+          'Referer': `https://jdsupermarket.jd.com/game/?tt=1597540727225`,
+          'Host': `api.m.jd.com`,
+          'Accept-Encoding': `gzip, deflate, br`,
+          'Accept-Language': `zh-cn`
+        }
+      }
+      $.post(url, async (err, resp, data) => {
+        try {
+          if (safeGet(data)) {
+            data = JSON.parse(data);
+            $.queryPrizeData = data;
+            if (data.data.bizCode !== 0) {
+              $.beanerr = `${data.data.bizMsg}`;
+              return
+            }
+            if (data.data.bizCode === 0) {
+              const {prizeList} = data.data.result;
+              if (`${coinToBeans}` === '1000') {
+                if (prizeList[1].beanType === 'BeanPackage') {
+                  console.log(`查询换${prizeList[1].title}ID成功，ID:${prizeList[1].prizeId}`)
+                  $.title = prizeList[1].title;
+                  $.blueCost = prizeList[1].blueCost;
+                } else {
+                  console.log(`查询换1000京豆ID失败`)
+                  $.beanerr = `东哥今天不给换`;
+                  return;
+                }
+                if (prizeList[1].inStock === 506) {
+                  $.beanerr = `失败，1000京豆领光了，请明天再来`;
+                  return;
+                }
+                if (prizeList[1].targetNum === prizeList[1].finishNum) {
+                  $.beanerr = `${prizeList[1].subTitle}`;
+                  return;
+                }
+                //兑换1000京豆
+                if ($.totalBlue > $.blueCost) {
+                  await smtg_obtainPrize(prizeList[1].prizeId);
+                } else {
+                  console.log(`兑换失败,您目前蓝币${$.totalBlue}个,不足以兑换${$.title}所需的${$.blueCost}个`);
+                  $.beanerr = `兑换失败,您目前蓝币${$.totalBlue}个,不足以兑换${$.title}所需的${$.blueCost}个`;
+                }
+              } else if (`${coinToBeans}` === '20') {
+                if (prizeList[0].beanType === 'Bean') {
+                  console.log(`查询换${prizeList[0].title}ID成功，ID:${prizeList[0].prizeId}`)
+                  $.title = prizeList[0].title;
+                  $.blueCost = prizeList[0].blueCost;
+                } else {
+                  console.log(`查询换万能的京豆ID失败`)
+                  $.beanerr = `东哥今天不给换`;
+                  return;
+                }
+                if (prizeList[0].inStock === 506) {
+                  console.log(`失败，万能的京豆领光了，请明天再来`);
+                  $.beanerr = `失败，万能的京豆领光了，请明天再来`;
+                  return;
+                }
+                if (prizeList[0].targetNum === prizeList[0].finishNum) {
+                  $.beanerr = `${prizeList[0].subTitle}`;
+                  return;
+                }
+                //兑换万能的京豆(1-20京豆)
+                if ($.totalBlue > $.blueCost) {
+                  await smtg_obtainPrize(prizeList[0].prizeId, 1000);
+                } else {
+                  console.log(`兑换失败,您目前蓝币${$.totalBlue}个,不足以兑换${$.title}所需的${$.blueCost}个`);
+                  $.beanerr = `兑换失败,您目前蓝币${$.totalBlue}个,不足以兑换${$.title}所需的${$.blueCost}个`;
+                }
+              } else {
+                //自定义输入兑换
+                let prizeId = '', i;
+                for (let index = 0; index < prizeList.length; index++) {
+                  if (prizeList[index].title.indexOf(coinToBeans) > -1) {
+                    prizeId = prizeList[index].prizeId;
+                    i = index;
+                    $.title = prizeList[index].title;
+                    $.blueCost = prizeList[index].blueCost;
+                  }
+                }
+                if (prizeId) {
+                  if (prizeList[i].inStock === 506) {
+                    console.log(`失败，您输入设置的${coinToBeans}领光了，请明天再来`);
+                    $.beanerr = `失败，您输入设置的${coinToBeans}领光了，请明天再来`;
+                    return;
+                  }
+                  if (prizeList[i].targetNum === prizeList[i].finishNum) {
+                    $.beanerr = `${prizeList[0].subTitle}`;
+                    return;
+                  }
+                  if ($.totalBlue > $.blueCost) {
+                    await smtg_obtainPrize(prizeId);
+                  } else {
+                    console.log(`兑换失败,您目前蓝币${$.totalBlue}个,不足以兑换${$.title}所需的${$.blueCost}个`);
+                    $.beanerr = `兑换失败,您目前蓝币${$.totalBlue}个,不足以兑换${$.title}所需的${$.blueCost}个`;
+                  }
+                } else {
+                  console.log(`奖品兑换列表[${coinToBeans}]已下架，请检查APP是否存在此商品，如存在请检查您的输入是否正确`);
+                  $.beanerr = `奖品兑换列表[${coinToBeans}]已下架`;
+                }
+              }
+            }
+          }
+        } catch (e) {
+          $.logErr(e, resp);
+        } finally {
+          resolve()
+        }
+      })
+    }, timeout)
+  })
+}
+
+//换京豆
+function smtg_obtainPrize(prizeId, timeout = 0) {
+  //1000京豆，prizeId为4401379726
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      let url = {
+        url: `${JD_API_HOST}&functionId=smtg_obtainPrize&clientVersion=8.0.0&client=m&body=%7B%22prizeId%22:%22${prizeId}%22%7D&t=${Date.now()}`,
+        headers: {
+          'Origin': `https://jdsupermarket.jd.com`,
+          'Cookie': cookie,
+          'Connection': `keep-alive`,
+          'Accept': `application/json, text/plain, */*`,
+          'Referer': `https://jdsupermarket.jd.com/game/?tt=1597540727225`,
+          'Host': `api.m.jd.com`,
+          'Accept-Encoding': `gzip, deflate, br`,
+          'Accept-Language': `zh-cn`
+        }
+      }
+      $.post(url, async (err, resp, data) => {
+        try {
+          console.log(`兑换结果:${data}`);
+          if (safeGet(data)) {
+            data = JSON.parse(data);
+            $.data = data;
+            if ($.data.data.bizCode !== 0) {
+              $.beanerr = `${$.data.data.bizMsg}`;
+              //console.log(`【京东账号${$.index}】${$.nickName} 换取京豆失败：${$.data.data.bizMsg}`)
+              return
+            }
+            if ($.data.data.bizCode === 0) {
+              if (`${coinToBeans}` === '1000') {
+                $.beanscount++;
+                console.log(`【京东账号${$.index}】${$.nickName} 第${$.data.data.result.exchangeNum}次换${$.title}成功`)
+                if ($.beanscount === 1) return;
+              } else if (`${coinToBeans}` === '20') {
+                $.beanscount++;
+                console.log(`【京东账号${$.index}】${$.nickName} 第${$.data.data.result.exchangeNum}次换${$.title}成功`)
+                if ($.data.data.result.exchangeNum === 20 || $.beanscount === coinToBeans || $.data.data.result.blue < 500) return;
+              } else {
+                $.beanscount++;
+                console.log(`【京东账号${$.index}】${$.nickName} 第${$.data.data.result.exchangeNum}次换${$.title}成功`)
+                if ($.beanscount === 1) return;
+              }
+            }
+          }
+          await smtg_obtainPrize(prizeId, 3000);
+        } catch (e) {
+          $.logErr(e, resp);
+        } finally {
+          resolve()
+        }
+      })
+    }, timeout)
+  })
+}
+
+function smtgHome() {
+  return new Promise((resolve) => {
+    $.get(taskUrl('smtg_home'), (err, resp, data) => {
+      try {
+        if (err) {
+          console.log('\n京小超兑换奖品: API查询请求失败 ‼️‼️')
+          console.log(JSON.stringify(err));
+        } else {
+          if (safeGet(data)) {
+            data = JSON.parse(data);
+            if (data.data.bizCode === 0) {
+              const {result} = data.data;
+              $.totalGold = result.totalGold;
+              $.totalBlue = result.totalBlue;
+              console.log(`【总金币】${$.totalGold}个\n`);
+              console.log(`【总蓝币】${$.totalBlue}个\n`);
+            }
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp);
+      } finally {
+        resolve();
+      }
+    })
+  })
+}
+
+//通知
+async function msgShow() {
+  // $.msg($.name, ``, `【京东账号${$.index}】${$.nickName}\n【收取蓝币】${$.coincount ? `${$.coincount}个` : $.coinerr }${coinToBeans ? `\n【兑换京豆】${ $.beanscount ? `${$.beanscount}个` : $.beanerr}` : ""}`);
+  $.log(`\n【京东账号${$.index}】${$.nickName}\n${coinToBeans ? `【兑换${$.title}】${$.beanscount ? `成功` : $.beanerr}` : "您设置的是不兑换奖品"}\n`);
+  let ctrTemp;
+  if ($.isNode() && process.env.MARKET_REWARD_NOTIFY) {
+    ctrTemp = `${process.env.MARKET_REWARD_NOTIFY}` === 'false';
+  } else if ($.getdata('jdSuperMarketRewardNotify')) {
+    ctrTemp = $.getdata('jdSuperMarketRewardNotify') === 'false';
+  } else {
+    ctrTemp = `${jdNotify}` === 'false';
+  }
+  //默认只在兑换奖品成功后弹窗提醒。情况情况加，只打印日志，不弹窗
+  if ($.beanscount && ctrTemp) {
+    $.msg($.name, ``, `【京东账号${$.index}】${$.nickName}\n${coinToBeans ? `【兑换${$.title}】${$.beanscount ? `成功，数量：${$.beanscount}个` : $.beanerr}` : "您设置的是不兑换奖品"}`);
+    if ($.isNode()) {
+      await notify.sendNotify(`${$.name} - 账号${$.index} - ${$.nickName}`, `【京东账号${$.index}】${$.UserName}\n${coinToBeans ? `【兑换${$.title}】${$.beanscount ? `成功，数量：${$.beanscount}个` : $.beanerr}` : "您设置的是不兑换奖品"}`)
+    }
+  }
+}
+
+function TotalBean() {
+  return new Promise(async resolve => {
+    const options = {
+      "url": `https://wq.jd.com/user/info/QueryJDUserInfo?sceneval=2`,
+      "headers": {
+        "Accept": "application/json,text/plain, */*",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-cn",
+        "Connection": "keep-alive",
+        "Cookie": cookie,
+        "Referer": "https://wqs.jd.com/my/jingdou/my.shtml?sceneval=2",
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1"
+      }
+    }
+    $.post(options, (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+          console.log(`${$.name} API请求失败，请检查网路重试`)
+        } else {
+          if (data) {
+            data = JSON.parse(data);
+            if (data['retcode'] === 13) {
+              $.isLogin = false; //cookie过期
+              return
+            }
+            $.nickName = data['base'].nickname;
+          } else {
+            console.log(`京东服务器返回空数据`)
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve();
+      }
+    })
+  })
+}
+
+function safeGet(data) {
+  try {
+    if (typeof JSON.parse(data) == "object") {
+      return true;
+    }
+  } catch (e) {
+    console.log(e);
+    console.log(`京东服务器访问数据为空，请检查自身设备网络情况`);
+    return false;
+  }
+}
+
+function taskUrl(function_id, body = {}) {
+  return {
+    url: `${JD_API_HOST}&functionId=${function_id}&clientVersion=8.0.0&client=m&body=${escape(JSON.stringify(body))}&t=${Date.now()}`,
+    headers: {
+      'User-Agent': 'jdapp;iPhone;9.0.8;13.6;Mozilla/5.0 (iPhone; CPU iPhone OS 13_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1',
+      'Host': 'api.m.jd.com',
+      'Cookie': cookie,
+      'Referer': 'https://jdsupermarket.jd.com/game',
+      'Origin': 'https://jdsupermarket.jd.com',
+    }
+  }
+}
---
+++ src/scripts/jd_daily_egg.js
@@ -0,0 +1,227 @@
+/*
+京东金融-天天提鹅
+定时收鹅蛋,兑换金币
+先这样子吧
+ */
+//0 */3 * * *
+const {Env} = require('../utils/Env')
+const $ = new Env('天天提鹅');
+let cookiesArr = [], cookie = '';
+const JD_API_HOST = 'https://ms.jr.jd.com/gw/generic/uc/h5/m';
+const notify = $.isNode() ? require('../utils/sendNotify') : '';
+let jdNotify = false;//是否开启推送互助码
+//Node.js用户请在jdCookie.js处填写京东ck;
+const jdCookieNode = $.isNode() ? require('../utils/jdCookie.js') : '';
+if ($.isNode()) {
+  Object.keys(jdCookieNode).forEach((item) => {
+    cookiesArr.push(jdCookieNode[item])
+  })
+  if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {
+  };
+} else {
+  cookiesArr.push(...[$.getdata('CookieJD'), $.getdata('CookieJD2')])
+}
+!(async () => {
+  if (!cookiesArr[0]) {
+    $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
+    return;
+  }
+  for (let i = 0; i < cookiesArr.length; i++) {
+    if (cookiesArr[i]) {
+      cookie = cookiesArr[i];
+      $.UserName = decodeURIComponent(cookie.match(/pt_pin=(.+?);/) && cookie.match(/pt_pin=(.+?);/)[1])
+      $.index = i + 1;
+      $.isLogin = true;
+      $.nickName = '';
+      await TotalBean();
+      console.log(`\n***********开始【京东账号${$.index}】${$.nickName || $.UserName}********\n`);
+      if (!$.isLogin) {
+        $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/`, {"open-url": "https://bean.m.jd.com/"});
+        $.setdata('', `CookieJD${i ? i + 1 : ""}`);//cookie失效，故清空cookie。
+        if ($.isNode()) await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
+        continue
+      }
+      message = '';
+      subTitle = '';
+      goodsUrl = '';
+      taskInfoKey = [];
+      option = {};
+      await jdDailyEgg();
+    }
+  }
+})()
+  .catch((e) => {
+    $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
+  })
+  .finally(() => {
+    $.done();
+  })
+
+async function jdDailyEgg() {
+  await toDailyHome()
+  await toWithdraw()
+  await toGoldExchange();
+}
+
+function toGoldExchange() {
+  return new Promise(async resolve => {
+    const body = {
+      "timeSign": 0,
+      "environment": "jrApp",
+      "riskDeviceInfo": "{}"
+    }
+    $.post(taskUrl('toGoldExchange', body), (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+          console.log(`${$.name} API请求失败，请检查网路重试`)
+        } else {
+          if (data) {
+            // console.log(data)
+            data = JSON.parse(data);
+            if (data.resultCode === 0) {
+              if (data.resultData.code === '0000') {
+                console.log(`兑换金币:${data.resultData.data.cnumber}`);
+                console.log(`当前总金币:${data.resultData.data.goldTotal}`);
+              } else if (data.resultData.code !== '0000') {
+                console.log(`兑换金币失败:${data.resultData.msg}`)
+              }
+            }
+          } else {
+            console.log(`京东服务器返回空数据`)
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve();
+      }
+    })
+  })
+}
+
+function toWithdraw() {
+  return new Promise(async resolve => {
+    const body = {
+      "timeSign": 0,
+      "environment": "jrApp",
+      "riskDeviceInfo": "{}"
+    }
+    $.post(taskUrl('toWithdraw', body), (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+          console.log(`${$.name} API请求失败，请检查网路重试`)
+        } else {
+          if (data) {
+            // console.log(data)
+            data = JSON.parse(data);
+            if (data.resultCode === 0) {
+              if (data.resultData.code === '0000') {
+                console.log(`收取鹅蛋:${data.resultData.data.eggTotal}个成功`);
+                console.log(`当前总鹅蛋数量:${data.resultData.data.userLevelDto.userHaveEggNum}`);
+              } else if (data.resultData.code !== '0000') {
+                console.log(`收取鹅蛋失败:${data.resultData.msg}`)
+              }
+            }
+          } else {
+            console.log(`京东服务器返回空数据`)
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve();
+      }
+    })
+  })
+}
+
+function toDailyHome() {
+  return new Promise(async resolve => {
+    const body = {
+      "timeSign": 0,
+      "environment": "jrApp",
+      "riskDeviceInfo": "{}"
+    }
+    $.post(taskUrl('toDailyHome', body), (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+          console.log(`${$.name} API请求失败，请检查网路重试`)
+        } else {
+          if (data) {
+            // console.log(data)
+            JSON.parse(data);
+          } else {
+            console.log(`京东服务器返回空数据`)
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve();
+      }
+    })
+  })
+}
+
+function TotalBean() {
+  return new Promise(async resolve => {
+    const options = {
+      "url": `https://wq.jd.com/user/info/QueryJDUserInfo?sceneval=2`,
+      "headers": {
+        "Accept": "application/json,text/plain, */*",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-cn",
+        "Connection": "keep-alive",
+        "Cookie": cookie,
+        "Referer": "https://wqs.jd.com/my/jingdou/my.shtml?sceneval=2",
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1"
+      }
+    }
+    $.post(options, (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+          console.log(`${$.name} API请求失败，请检查网路重试`)
+        } else {
+          if (data) {
+            data = JSON.parse(data);
+            if (data['retcode'] === 13) {
+              $.isLogin = false; //cookie过期
+              return
+            }
+            $.nickName = data['base'].nickname;
+          } else {
+            console.log(`京东服务器返回空数据`)
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve();
+      }
+    })
+  })
+}
+
+function taskUrl(function_id, body) {
+  return {
+    url: `${JD_API_HOST}/${function_id}`,
+    body: `reqData=${encodeURIComponent(JSON.stringify(body))}`,
+    headers: {
+      'Accept': `application/json`,
+      'Origin': `https://uua.jr.jd.com`,
+      'Accept-Encoding': `gzip, deflate, br`,
+      'Cookie': cookie,
+      'Content-Type': `application/x-www-form-urlencoded;charset=UTF-8`,
+      'Host': `ms.jr.jd.com`,
+      'Connection': `keep-alive`,
+      'User-Agent': `jdapp;iPhone;9.0.0;13.4.1;e35caf0a69be42084e3c97eef56c3af7b0262d01;network/4g;ADID/F75E8AED-CB48-4EAC-A213-E8CE4018F214;supportApplePay/3;hasUPPay/0;pushNoticeIsOpen/1;model/iPhone11,8;addressid/2005183373;hasOCPay/0;appBuild/167237;supportBestPay/0;jdSupportDarkMode/0;pv/1287.19;apprpd/MyJD_GameMain;ref/https%3A%2F%2Fuua.jr.jd.com%2Fuc-fe-wxgrowing%2Fmoneytree%2Findex%2F%3Fchannel%3Dyxhd%26lng%3D113.325843%26lat%3D23.204628%26sid%3D2d98e88cf7d182f60d533476c2ce777w%26un_area%3D19_1601_50258_51885;psq/1;ads/;psn/e35caf0a69be42084e3c97eef56c3af7b0262d01|3485;jdv/0|kong|t_1000170135|tuiguang|notset|1593059927172|1593059927;adk/;app_device/IOS;pap/JA2015_311210|9.0.0|IOS 13.4.1;Mozilla/5.0 (iPhone; CPU iPhone OS 13_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1`,
+      'Referer': `https://uua.jr.jd.com/uc-fe-wxgrowing/moneytree/index`,
+      'Accept-Language': `zh-cn`
+    }
+  }
+}
