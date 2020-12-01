660e1d39fc9bce6a033958e52ead6431c6a39691
jake3737 <50729332+jake3737@users.noreply.github.com>
2020-12-01 14:16:45 +0800

    Add files via upload

--- ShareCode.js
+++ ShareCode.js
@@ -49,61 +49,25 @@ const shareCodes = [
         ''
     ),
     new ShareCode(
-        '大咪嗷嗷1',
-        '987ee04888b64f0597b13049853caca4',
-        'MTE1NDAxNzgwMDAwMDAwNDAwMzg2Mjk=',
-        'bknudbr7e4sqwbhh4kxjg7vthwtj3ctybaw66uy',
-        'P04z54XCjVWnYaS5m9cZxa_rCUbw1uYn51X5g',
+        '遇见3未',
+        'd92cbd8f7c0048699452db1fdc24a75b',
+        'MTAxODc2NTEzMTAwMDAwMDAyODc2NjM2NQ==',
+        'zlmjgctb673yvqj464zq6mwdee',
+        'P04z54XCjVWnYaS5mlbSWb51Q',
     ),
     new ShareCode(
-        '高敏',
-        '32b5348df6374239964194e187fa187c',
-        'MTE1NDUwMTI0MDAwMDAwMDQwNDcxOTYx',
-        'mv3riww4sqy5f53abjy63rud3qz6gsx6f7h3czq',
+        '1jake37',
+        'd9e3d1ffadd34ab9b5e990010d258c60',
+        'MTE1NDAxNzcwMDAwMDAwMzg2MDE0Njc=',
+        '4larfd6ua4ecyw5gp5ojqcdz57wfcewgnlpjfgq',
     ),
     new ShareCode(
-        '大咪嗷嗷2',
-        '97d2efe84bd74bf8aacacb9b9bac20fd',
-        'MTEzMzI0OTE0NTAwMDAwMDA0MTU3ODg3OQ==',
-        'gou7sxm3hztwosdrlp4mnrat6ypjtw3b5ukpqpq',
-        'P04z54XCjVWnYaS5m9cZyaNjzQOxlCuj5XW6w',
-        'tvUf4vUpkLF9KyitjqKI3A=='
-    ),
-    new ShareCode(
-        '大咪嗷嗷3',
-        undefined,
-        undefined,
-        'pt_key=AAJfw5oZADCBdZciQl2_tzrlQQ3HYQYfbREj3YIOioHQoGyjc4-G4tgqb6F4NdHKEpyhiY7ar1g; pt_pin=jd_KWhQIuTtixQJ;'
-    ),
-    new ShareCode(
-        '查钧译',
-        '9a07bef4e7ad49ae8fdaf113de817a1b',
-        'MTE1NDUyMjEwMDAwMDAwNDAwMTQ1NjM=',
-        'mlrdw3aw26j3xhxesrqelkqhmqott5jmflm63ha'
-    ),
-    new ShareCode(
-        '刘奕',
-        '800d1ec39dbd473bbcf2f35667b78b02',
-        'MTAxODc2NTEzNTAwMDAwMDAyOTAxMTkwMQ==',
-        'q74cnfebbilqdntxbtix6cx5a4'
-    ),
-    new ShareCode(
-        '查钧译的老婆',
-        'c168e10ee28248e79defbf76bd098e35',
-        'MTE1NDQ5OTIwMDAwMDAwNDAxNjc4MzU=',
-        'mlrdw3aw26j3wscxylsmq7u37f2s7oeysmjexxa'
-    ),
-    new ShareCode(
-        '刘奕1',
-        'd241a66537244934ba66d40701231a82',
-        'MTE1NDAxNzgwMDAwMDAwNDAzMDE1Mzk=',
-        '4npkonnsy7xi3utpy37pb3qc2vwdg4im4ts6rfa'
-    ),
-    new ShareCode(
-        'hema3210',
-        '4cbf9b4070b14efa853ee3e3ea0a97db',
-        'MTE1NDUyMjEwMDAwMDAwNDAwMzgwOTU=',
-        'lqvulo4mnacvkub4mrvr2u3qba'
+        'jd_62433',
+        'c162b16c03b846ba9a7cd98448c29025',
+        'MTE1NDUwMTI0MDAwMDAwMDM4OTU1ODYx',
+        'xmblfgd3rmwo46hdnzy3mnvb7tmnzawk2kmrffi',
+        'P04z54XCjVWnYaS5m9cZzKKhB4y_X4oQNFVDw',
+        'jxiWiwhUmfE47jIWiiZrYQ=='
     )
 ]
 
