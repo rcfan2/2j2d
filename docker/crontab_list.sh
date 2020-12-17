8eb3ea4193ce70b5dfadefeef25a9def3c7a799a
jake3737 <50729332+jake3737@users.noreply.github.com>
2020-12-17 12:42:46 +0800

    Add files via upload

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -1,16 +1,21 @@
-50 */1 * * * git -C /scripts/ pull >> /scripts/logs/pull.log 2>&1
-52 */1 * * * crontab /scripts/docker/${CRONTAB_LIST_FILE}
-53 */1 * * * npm install --prefix /scripts >> /scripts/logs/npm_install.log 2>&1
+#必须要的默认定时任务请勿删除
+52 */1 * * * sh /scripts/docker/default_task.sh >> /scripts/logs/default_task.log 2>&1
 # 每3天的23:50分清理一次日志
 50 23 */3 * * rm -rf /scripts/logs/*.log
 
-
 ##############短期活动##############
-# 暂无
-
+# 秒杀红包雨(2020.12.31活动过期)
+40 8 1-31 12 * node /scripts/jd_ms_redrain.js >> /scripts/logs/jd_ms_redrain.log 2>&1
+# 健康抽奖机(2020.12.31活动过期)
+10 0 1-31 12 * node /scripts/jd_health.js >> /scripts/logs/jd_health.log 2>&1
+# 直播红包雨(2020.12.31活动过期)
+1 0,20,9-23/2 15-31 12 * node /scripts/jd_live_redrain.js >> /scripts/logs/jd_live_redrain.log 2>&1
+# 京东金融打卡领年终奖(2020.12.31活动过期)
+10 6 1-31 12 * node /scripts/jr_sign.js >> /scripts/logs/jr_sign.log 2>&1
+# 京东健康APP集汪汪卡瓜分百万红包(2021.1.6活动过期)
+10 8 * * * node /scripts/jd_jdh.js >> /scripts/logs/jd_jdh.log 2>&1
 
 ##############长期活动##############
-
 # 签到
 0 0,12,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
 # 京小超兑换奖品
@@ -61,5 +66,19 @@
 16 6 * * * node /scripts/jd_small_home.js >> /scripts/logs/jd_small_home.log 2>&1
 # 东东工厂
 36 * * * * node /scripts/jd_jdfactory.js >> /scripts/logs/jd_jdfactory.log 2>&1
-#十元街
-36 8 * * * node /scripts/jd_syj.js >> /scripts/logs/jd_syj.log 2>&1
+# 十元街
+36 8,18 * * * node /scripts/jd_syj.js >> /scripts/logs/jd_syj.log 2>&1
+# 京东代属(注:限校园用户可使用)
+36 9 * * * node /scripts/jd_ds.js >> /scripts/logs/jd_ds.log 2>&1
+# 京东快递签到
+23 1 * * * node /scripts/jd_kd.js >> /scripts/logs/jd_kd.log 2>&1
+# 京东汽车(签到满500赛点可兑换500京豆)
+33 2 * * * node /scripts/jd_car.js >> /scripts/logs/jd_car.log 2>&1
+# 领京豆额外奖励(每日可获得3京豆)
+33 4 * * * node /scripts/jd_bean_home.js >> /scripts/logs/jd_bean_home.log 2>&1
+# 京东直播(每日18豆)
+10-20/5 11 * * * node /scripts/jd_live.js >> /scripts/logs/jd_live.log 2>&1
+#微信小程序京东赚赚
+10 11 * * * node /scripts/jd_jdzz.js >> /scripts/logs/jd_jdzz.log 2>&1
+#宠汪汪邀请助力
+10 10,11 * * * node /scripts/jd_joy_run.js >> /scripts/logs/jd_joy_run.log 2>&1
