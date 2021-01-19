6c2b588c079a4c642e0f54c4d01c2c134368d443
jake3737 <50729332+jake3737@users.noreply.github.com>
2021-01-19 14:01:20 +0800

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -28,7 +28,7 @@ let cookiesArr = [], cookie = '', jdFruitShareArr = [], isBox = false, notify, n
 //此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // 这个列表填入你要助力的好友的shareCode
-  'f8df73267c104df2acba96d9378f06c5@987ee04888b64f0597b13049853caca4@32b5348df6374239964194e187fa187c@1f54af63767247c8bee99a03d287aff3@97d2efe84bd74bf8aacacb9b9bac20fd@6831e59df38e40468b099ff48e6a1a0f@0b51c67459b74f288e39b5ee091ffe1a@800d1ec39dbd473bbcf2f35667b78b02@9a07bef4e7ad49ae8fdaf113de817a1b@c168e10ee28248e79defbf76bd098e35@d241a66537244934ba66d40701231a82',
+  'c162b16c03b846ba9a7cd98448c29025@d92cbd8f7c0048699452db1fdc24a75b@102ea0f241a84f77944b39c37efef174',
 ]
 let message = '', subTitle = '', option = {}, isFruitFinished = false;
 const retainWater = 100;//保留水滴大于多少g,默认100g;
