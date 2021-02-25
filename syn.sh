15af062e28d1d1c0e83bd0c7d0fa6059c6ae1af0
tracefish <64358864+tracefish@users.noreply.github.com>
2021-02-25 17:49:35 +0800

    Update syn.sh

--- syn.sh
+++ syn.sh
@@ -12,10 +12,10 @@ docker-compose up -d
 #docker-compose pull
 
 echo "设定远程仓库地址..."
-docker exec -i jd_scripts /bin/sh -c "git remote set-url origin $REPO_URL"
+#docker exec -i jd_scripts /bin/sh -c "git remote set-url origin $REPO_URL"
 echo "git pull拉取最新代码..."
-docker exec -i jd_scripts /bin/sh -c 'git fetch --all'
 docker exec -i jd_scripts /bin/sh -c 'git reset --hard'
+docker exec -i jd_scripts /bin/sh -c 'git fetch --all'
 ls -lR
 docker images
 #docker save `docker images | grep latest | grep -v grep | awk '{print $3}'` > ~/jd.tar
