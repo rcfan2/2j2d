a19940c463c00ad7c96463dc884c68ace6654638
tracefish <64358864+tracefish@users.noreply.github.com>
2021-02-25 19:22:53 +0800

    Update syn.sh

--- syn.sh
+++ syn.sh
@@ -15,7 +15,7 @@ echo "设定远程仓库地址..."
 docker exec -i jd_scripts /bin/sh -c "git remote set-url origin $REPO_URL"
 echo "git pull拉取最新代码..."
 docker exec -i jd_scripts /bin/sh -c 'git stash'
-docker exec -i jd_scripts /bin/sh -c "git -C ~/scripts pull $REPO_URL"
+docker exec -i jd_scripts /bin/sh -c "mkdir ~/scripts && git -C ~/scripts pull $REPO_URL"
 ls -lR
 docker images
 #docker save `docker images | grep latest | grep -v grep | awk '{print $3}'` > ~/jd.tar
