fa85ae7a55659d3d609544353292d90c045f9f5e
tracefish <64358864+tracefish@users.noreply.github.com>
2021-02-26 01:17:58 +0800

    Update syn.sh

--- syn.sh
+++ syn.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env sh
 
-set -e
+#set -e
 
+#mkdir -p ~/jd_scripts/logs
+#cp -f docker-compose.yml ~/jd_scripts/
+#mv -f crontab_list.sh ~/jd_scripts/my_crontab_list.sh
+#cd ~/jd_scripts/
 docker rmi `docker images -q`
 echo "Get docker image"
 docker pull $SOURCE_IMAGE
+#docker-compose up -d
+#docker-compose logs
+#docker-compose pull
+
+#echo "设定远程仓库地址..."
+#docker exec -i jd_scripts /bin/sh -c "git remote set-url origin $REPO_URL"
+#echo "git pull拉取最新代码..."
+#docker exec -i jd_scripts /bin/sh -c "git reset --hard origin/master"
+
+#docker exec -i jd_scripts /bin/sh -c "git pull --rebase"
+echo "修改docker脚本"
+for file in `sudo find /var/lib/docker -type f -name "docker_entrypoint.sh" | grep -v "scripts/docker/docker_entrypoint.sh"`
+do
+    sudo sed -i "s/npm/#npm/g" $file && sudo sed -i "s/sh -x/#sh/g" $file && sudo sed -i "s/crond/#/g" $file
+done
+
+docker images
+docker run -i $SOURCE_IMAGE /bin/sh >/dev/null
 
 docker images
 docker save `docker images | grep latest | grep -v grep | awk '{print $3}'` > ~/jd.tar
@@ -19,10 +41,16 @@ done
 
 cd ~/scripts/scripts/
 SOURCE_BRANCH="master"
-SOURCE_BRANCH=`git branch | awk '{print $2}'`
+#SOURCE_BRANCH=`git branch | awk '{print $2}'`
+#UPSTREAM_REPO=`git remote -v | grep origin | grep fetch | awk '{print $2}'`
+git remote --verbose
 
 #REPO_URL="git@gitee.com:lxk0301/jd_scripts.git"
-
+#git remote set-url origin $REPO_URL
+#git reset --hard
+#echo "git pull拉取最新代码..."
+#git pull --rebase
+git branch -a
 echo "Resetting origin to: https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
 git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
 git remote --verbose
