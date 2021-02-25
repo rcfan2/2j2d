646e90453dc979dca6e5ee0b52e3e9e8795c263a
tracefish <64358864+tracefish@users.noreply.github.com>
2021-02-25 22:38:06 +0800

    Update syn.sh

--- syn.sh
+++ syn.sh
@@ -20,7 +20,8 @@ docker pull $SOURCE_IMAGE
 
 #docker exec -i jd_scripts /bin/sh -c "git pull --rebase"
 docker images
-docker run -id $SOURCE_IMAGE /bin/sh -c "git pull"
+docker run -id $SOURCE_IMAGE /bin/sh
+docker exec -i $SOURCE_IMAGE /bin/sh -c "git pull"
 #docker save `docker images | grep latest | grep -v grep | awk '{print $3}'` > ~/jd.tar
 #[ ! -e ~/scripts ] && mkdir ~/scripts && tar xvf ~/jd.tar -C ~/scripts
 
