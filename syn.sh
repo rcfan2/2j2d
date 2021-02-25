5dcfc0ed432b43aca9d6ea3cdd85a26492463a8e
tracefish <64358864+tracefish@users.noreply.github.com>
2021-02-25 15:41:15 +0800

    Update syn.sh

--- syn.sh
+++ syn.sh
@@ -5,6 +5,7 @@ set -e
 docker rmi `docker images -q`
 echo "Get docker image"
 docker pull $SOURCE_IMAGE
+docker-compose up -d
 docker save `docker images | grep latest | grep -v grep | awk '{print $3}'` > ~/jd.tar
 [ ! -e ~/scripts ] && mkdir ~/scripts && tar xvf ~/jd.tar -C ~/scripts
 
@@ -20,11 +21,11 @@ SOURCE_BRANCH=`git branch | awk '{print $2}'`
 #UPSTREAM_REPO=`git remote -v | grep origin | grep fetch | awk '{print $2}'`
 git remote --verbose
 
-REPO_URL="git@gitee.com:lxk0301/jd_scripts.git"
-git remote set-url origin $REPO_URL
-git reset --hard
-echo "git pull拉取最新代码..."
-git pull --rebase
+#REPO_URL="git@gitee.com:lxk0301/jd_scripts.git"
+#git remote set-url origin $REPO_URL
+#git reset --hard
+#echo "git pull拉取最新代码..."
+#git pull --rebase
 
 echo "Resetting origin to: https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
 git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
