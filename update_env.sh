ce72c0bc2db097baeda4d9c421216e7110a0248b
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-01 14:50:42 +0800

    Create update_env.sh

---
+++ update_env.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "拉取源码"
+git clone -b jd https://github.com/tracefish/ds.git ~/scripts
+cd  ~/scripts
+echo "修改源码"
+sed -i "s/indexOf('GITHUB')/indexOf('GOGOGOGO')/g" `ls -l |grep -v ^d|awk '{print $9}'`
+sed -i 's/indexOf("GITHUB")/indexOf("GOGOGOGO")/g' `ls -l |grep -v ^d|awk '{print $9}'`
+echo "修改文件"
+git clone -b sc https://github.com/tracefish/ds.git ./sharecode
+sed -i '2i\process.env.SHARE_CODE_FILE = ".\/sharecode\/sharecode.log";' ./utils/jdShareCodes.js
+npm install
