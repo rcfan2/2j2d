786208720925270c6f4869f9e48806499842ba6c
jake3737 <50729332+jake3737@users.noreply.github.com>
2021-03-03 20:41:20 +0800

    Update HelpSelf.js

--- HelpSelf.js
+++ HelpSelf.js
@@ -14,7 +14,7 @@ const invalidCookies=[]
 const JD_API_HOST = 'https://api.m.jd.com/client.action'
 
 !(async () => {
-  await getCookies('https://raw.githubusercontent.com/jake3737/2j2d/master/Cookie.json')
+  await getCookies('https://raw.githubusercontent.com/jake3737/zyjd/master/Cookie.json')
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
