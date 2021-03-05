617eecc94786bd6a7babab2fe2fd010d2cc60ad7
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-05 08:13:51 +0800

    Update concurrency.sh

--- concurrency.sh
+++ concurrency.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 # 多账号并发,不定时
 # 变量：要运行的脚本$SCRIPT
+set -e
 SCRIPT="$1"
 JD_COOKIES="$JD_COOKIES"
 echo "开始多账号并发"
 IFS=$'\n'
 num=0
-for jk in `echo "$JD_COOKIES" | awk -F "&" '{for(i=1;i<=NF;i++) print $i}'`;do cp  -rf ~/scripts ~/scripts${num} && \
-cd ~/scripts${num}
-sed -i 's/let CookieJDs/let CookieJDss/g' ./jdCookie.js && \
-sed -i "1i\let CookieJDs = [ '$jk', ]" ./jdCookie.js && \
-node "./${SCRIPT}" &
-cd ~
-num=$((num + 1))
+for jk in `echo "$JD_COOKIES" | awk -F "&" '{for(i=1;i<=NF;i++) print $i}'`
+do
+  cp  -rf ~/scripts ~/scripts${num}
+  cd ~/scripts${num}
+  sed -i 's/let CookieJDs/let CookieJDss/g' ./jdCookie.js
+  sed -i "1i\let CookieJDs = [ '$jk', ]" ./jdCookie.js
+  node "./${SCRIPT}" &
+  cd ~
+  num=$((num + 1))
 done
 unset IFS
 wait
