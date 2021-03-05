d5461c2b6d41411368d5df209d01857310fe562d
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-05 18:24:57 +0800

    Update concurrency.sh

--- concurrency.sh
+++ concurrency.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # 多账号并发,不定时
 # 变量：要运行的脚本$SCRIPT
+# 默认随机延迟5-12秒
 set -e
 SCRIPT="$1"
+DELAY="$2"
+
 echo "开始多账号并发"
 IFS=$'\n'
 JK_LIST=(`echo "$JD_COOKIES" | awk -F "&" '{for(i=1;i<=NF;i++) print $i}'`)
@@ -15,6 +18,11 @@ do
   sed -i "1i\let CookieJDs = [ '$jk', ]" ./jdCookie.js
   node "./${SCRIPT}" &
   cd ~
+  # 随机延迟5-12秒
+  random_time=$(($RANDOM%12+5))
+  delay=${DELAY:-$random_time}
+  echo "随机延迟${delay}秒"
+  sleep ${delay}s
   num=$((num + 1))
 done
 echo "有账号" "${num}"
