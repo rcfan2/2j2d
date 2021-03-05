751eddd582371e13675eb1d8363ebcc9225b126e
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-05 10:10:29 +0800

    Update concurrency.sh

--- concurrency.sh
+++ concurrency.sh
@@ -7,13 +7,12 @@ echo "开始多账号并发"
 IFS=$'\n'
 JK_LIST=(`echo "$JD_COOKIES" | awk -F "&" '{for(i=1;i<=NF;i++) print $i}'`)
 num=0
-for jk in "${JK_LIST[*]}"
+for jk in ${JK_LIST[*]}
 do
   cp  -rf ~/scripts ~/scripts${num}
   cd ~/scripts${num}
   sed -i 's/let CookieJDs/let CookieJDss/g' ./jdCookie.js
-  echo "($jk)"
-  sed -i "1i\let CookieJDs = [ '$jk', ]" ./jdCookie.js
+  sed -i "1i\let CookieJDs = [ '($jk)', ]" ./jdCookie.js
   #node "./${SCRIPT}" &
   cd ~
   num=$((num + 1))
