6366867771e1da77034c4bd13bd3067f3a8cb73b
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-05 11:49:34 +0800

    Create checkout.sh

---
+++ checkout.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+set -e
+
+mkdir -p ~/jd_scripts/
+git clone https://gitee.com/twinzo/scripts/ ~/jd_scripts
+cd ~/jd_scripts/
+
+echo "git 回退"
+git checkout 67d87e487b89dc0f060c0cf9bc2523a4e95cab98
+
+cd ~/jd_scripts
+#UPSTREAM_REPO=`git remote -v | grep origin | grep fetch | awk '{print $2}'`
+git remote --verbose
+
+echo "Resetting origin to: https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
+sudo git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
+git remote --verbose
+
+echo "Pushing changings from tmp_upstream to origin"
+sudo git push origin "refs/remotes/origin/${SOURCE_BRANCH}:refs/heads/${DESTINATION_BRANCH}" --force
+
+git remote --verbose
