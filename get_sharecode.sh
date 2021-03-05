4239814d8e6c7874903a49f3aadd7bd59bca30b2
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-05 21:01:24 +0800

    Update get_sharecode.sh

--- get_sharecode.sh
+++ get_sharecode.sh
@@ -3,24 +3,26 @@
 set -e
 echo "获得本目录下各脚本位置"
 git clone $REPO_URL -b $BRANCH ~/repo
-cd ~/repo/.github/workflows     
-echo "### 活动脚本位置" > ~/sctipts.md
+cd ~/repo/.github/workflows
+SC_LOG="sharecode.log"
+LOCATION_LOG="sctipts.md"
+echo "### 活动脚本位置" > ~/$LOCATION_LOG
 for file in `ls ./`; do
   isScript=`cat "$file" | grep "jd*.js"`
   if [ -n "$isScript" ]; then
-    echo -e "$file" >> ~/sctipts.md
-    echo \`\`\` >> ~/sctipts.md
-    echo "$isScript" >> ~/readme.md
-    echo \`\`\` >> ~/sctipts.md
+    echo -e "$file" >> ~/$LOCATION_LOG
+    echo \`\`\` >> ~/$LOCATION_LOG
+    echo "$isScript" >> ~/$LOCATION_LOG
+    echo \`\`\` >> ~/$LOCATION_LOG
   fi
 done
 
 echo "开始更新助力码"
 cd  ~/scripts
-node ./jd_get_share_code.js > ~/sharecode.log
-sed -i 's/京东农场/东东农场/g' ~/sharecode.log
-sed -i 's/种豆得豆/京东种豆得豆/g' ~/sharecode.log
-cat ~/sharecode.log
+node ./jd_get_share_code.js > ~/$SC_LOG
+sed -i 's/京东农场/东东农场/g' ~/$SC_LOG
+sed -i 's/种豆得豆/京东种豆得豆/g' ~/$SC_LOG
+cat ~/$SC_LOG
 echo "克隆指定仓库分支"
 git clone -b $DESTINATION_BRANCH $REPO_URL ~/ds
 cd ~/ds
@@ -28,8 +30,8 @@ echo "Resetting origin to: https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHU
 sudo git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
 
 echo "强制覆盖原文件"
-mv -f ~/sharecode.log ./sharecode.log
-mv -v ~/sctipts.md ./sctipts.md
+mv -f ~/sharecode.log ./$SC_LOG
+mv -v ~/$LOCATION_LOG./$LOCATION_LOG
 git config --global user.email "tracefish@qq.com"
 git config --global user.name "tracefish"
 git add .
