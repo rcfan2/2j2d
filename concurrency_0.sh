bb7000045035399c96e9ab57558d7d3b0b16934e
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-05 11:07:11 +0800

    Update concurrency_0.sh

--- concurrency_0.sh
+++ concurrency_0.sh
@@ -11,13 +11,17 @@ echo "开始多账号并发"
 IFS=$'\n'
 num=0
 nextdate=`date +%s%N -d "+1 day 00:00:00"`
-for jk in `echo "$JD_COOKIES" | awk -F "&" '{for(i=1;i<=NF;i++) print $i}'`;do cp  -rf ~/scripts ~/scripts${num} && \
-cd ~/scripts${num}
-sed -i 's/let CookieJDs/let CookieJDss/g' ./jdCookie.js && \
-sed -i "1i\let CookieJDs = [ '$jk', ]" ./jdCookie.js && \
-(now=`date +%s%N` && delay=`echo "scale=3;$((nextdate-now))/1000000000" | bc` && echo "未到当天零点，等待${delay}秒" && sleep $delay && node ./$SCRIPT)&
-cd ~
-num=$((num + 1))
+JK_LIST=(`echo "$JD_COOKIES" | awk -F "&" '{for(i=1;i<=NF;i++) print $i}'`)
+num=0
+for jk in ${JK_LIST[*]}
+do 
+  cp  -rf ~/scripts ~/scripts${num}
+  cd ~/scripts${num}
+  sed -i 's/let CookieJDs/let CookieJDss/g' ./jdCookie.js
+  sed -i "1i\let CookieJDs = [ '$jk', ]" ./jdCookie.js
+  (now=`date +%s%N` && delay=`echo "scale=3;$((nextdate-now))/1000000000" | bc` && echo "未到当天零点，等待${delay}秒" && sleep $delay && node ./$SCRIPT)&
+  cd ~
+  num=$((num + 1))
 done
 unset IFS
 wait
