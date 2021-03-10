47d6e88517be386d99dd4bf9aa3eb90a5a7a0167
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-10 17:10:32 +0800

    Update update_env.sh

--- update_env.sh
+++ update_env.sh
@@ -8,4 +8,6 @@ sed -i 's/indexOf("GITHUB")/indexOf("GOGOGOGO")/g' `ls -l |grep -v ^d|awk '{prin
 echo "修改文件"
 git clone -b sc https://github.com/tracefish/ds.git ./sharecode
 sed -i '2i\process.env.SHARE_CODE_FILE = ".\/sharecode\/sharecode.log";' ./utils/jdShareCodes.js
+#echo "修改助力码"
+#SGMH_SHARECODES=`cat ~/sharecode.log | while read LINE; do echo $LINE; done | grep "闪购盲盒"| awk -F "】" '{printf $2"@"}'`
 npm install
