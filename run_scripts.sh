337d9e7a5f7e5a2d6eaffcbc56008ccc90182883
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-14 00:36:09 +0800

    Update run_scripts.sh

--- run_scripts.sh
+++ run_scripts.sh
@@ -39,7 +39,7 @@ echo "替换助力码"
 autoHelp "${1}" "${logDir}/${SCRIPT_NAME}.log"
 echo "开始运行"
 node $1 >&1 | tee ${LOG}
-
+cat "$1"
 # 收集助力码
 collectSharecode(){
     echo "${1}：收集新助力码"
@@ -48,6 +48,7 @@ collectSharecode(){
         activity=`sed -n '/配置文件.*/'p "${LOG}" | awk -F "获取" '{print $2}' | awk -F "配置" '{print $1}'`
         name=(`sed -n '/'【京东账号'.*/'p "${LOG}" | grep "开始" | awk -F "开始" '{print $2}' |sed 's/】/（/g'| awk -v ac="$activity" -F "*" '{print $1"）" ac "好友助力码】"}'`)
         code=(`sed -n '/'您的好友助力码为'.*/'p ${1} | awk '{print $2}'`)
+        [ -z "$code" ] && exit 0
         for i in `seq 0 $((${#name[*]}-1))`
         do 
             echo "${name[i]}""${code[i]}" >> ${LOG}1
