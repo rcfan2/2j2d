761f09dac209f2bac9ef47f50fc0f0e313f148a2
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-13 12:38:06 +0800

    Update run_scripts.sh

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,10 +1,11 @@
-LOG="./${1}.log"
+LOG="./`echo "${1}" | awk -F "." '{print $1}'`.log"
 cd ~/scripts
 node $1 >&1 | tee ${LOG}
 
 # 收集助力码
 collectSharecode(){
     echo "${1}：收集新助力码"
-    sed -n '/'${1}'.*/'p ${1} |  sed 's/京东账号/京东账号 /g' | sed 's/（/ （/g' | sed 's/】/】 /g' | awk '{print $4,$5,$6,$7}' | sort -gk2  | awk '!a[$2" "$3]++{print}' >> ${LOG}1
+    sed -n '/'码】'.*/'p ${1} >> ${LOG}1
 }
 collectSharecode ${LOG}
+cat ${LOG}1
