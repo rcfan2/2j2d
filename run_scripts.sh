299d2b23184d136bb0dcf9c0d111b21cda90e3b8
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-13 11:44:08 +0800

    Create run_scripts.sh

---
+++ run_scripts.sh
@@ -0,0 +1,9 @@
+LOG="./${1}.log"
+node $1 >&1 | tee ${LOG}
+
+# 收集助力码
+collectSharecode(){
+    echo "${1}：收集新助力码"
+    sed -n '/'${1}'.*/'p ${1} |  sed 's/京东账号/京东账号 /g' | sed 's/（/ （/g' | sed 's/】/】 /g' | awk '{print $4,$5,$6,$7}' | sort -gk2  | awk '!a[$2" "$3]++{print}' >> ${LOG}1
+}
+collectSharecode ${LOG}
