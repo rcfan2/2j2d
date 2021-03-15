0217aad8c21b1660975aa1ef9d366605d8c9258f
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-15 11:41:10 +0800

    Update get_sharecode.sh

--- get_sharecode.sh
+++ get_sharecode.sh
@@ -45,7 +45,7 @@ cd  ~/scripts
 echo "克隆指定仓库分支"
 git clone -b $DESTINATION_BRANCH $REPO_URL ~/ds
 cd ~/ds
-code_aboard "jdcash" "./jd_cash.log"
+code_aboard "jdcash" "~/ds/jd_cash.log"
 
 echo "Resetting origin to: https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
 sudo git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
