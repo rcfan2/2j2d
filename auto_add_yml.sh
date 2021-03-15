2d682e39077e4ce5eb2d74898a0f8ee09cb42c6f
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-15 23:51:44 +0800

    Update auto_add_yml.sh

--- auto_add_yml.sh
+++ auto_add_yml.sh
@@ -5,8 +5,8 @@ git checkout sc
 
 ex_list=(`cat ./scripts.md | grep -v "*" | grep jd_`)
 # 忽略列表
-ig_list=(jd_family jd_delCoupon jd_get_share_code jx_nc)
-al_list=(${ex_list[*]}+${ig_list[*]})
+ig_list=(jd_family jd_delCoupon jd_get_share_code jd_jxnc)
+al_list=(${ex_list[*]} ${ig_list[*]})
 echo "${al_list[*]}"
 grep_list=`echo ${al_list[*]} | awk '{for(i=1;i<=NF;i++) print $i".*"}'`
 git checkout jd
