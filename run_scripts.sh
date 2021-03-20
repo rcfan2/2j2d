afed3246f439e1ea8403cd22dde7250808fe38c5
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-20 15:13:07 +0800

    Update run_scripts.sh

--- run_scripts.sh
+++ run_scripts.sh
@@ -75,6 +75,7 @@ collectSharecode(){
         activity=`sed -n '/配置文件.*/'p "${LOG}" | awk -F "获取" '{print $2}' | awk -F "配置" '{print $1}'`
         name=(`sed -n '/'【京东账号'.*/'p "${LOG}" | grep "开始" | awk -F "开始" '{print $2}' |sed 's/】/（/g'| awk -v ac="$activity" -F "*" '{print $1"）" ac "好友助力码】"}'`)
         code=(`sed -n '/'您的好友助力码为'.*/'p ${1} | awk '{print $2}'`)
+        [ -z "$code" ] && code=(`sed -n '/'好友助力码'.*/'p ${1} | awk -F "：" '{print $2}'`)
         [ -z "$code" ] && exit 0
         for i in `seq 0 $((${#name[*]}-1))`
         do 
